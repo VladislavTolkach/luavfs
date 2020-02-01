ced43e92cfea4411d9940138cf79c56f04fcdf4c
Vladislav Tolkach <vladislavtolkach2@gmail.com>
2020-02-02 02:43:05 +0300

    memfs: Implement file.new method

--- src/memfs/file.lua
+++ src/memfs/file.lua
@@ -2,36 +2,36 @@ local fdata = require("fdata")
 
 local file_m = {}
 
-local File = {}
-
 
 local function write(file, data)
-   local n = file.node
-   local len = fdata.write(n.data, file.pos, data)
-   file.pos = file.pos + len
+   local n = file._node
+   local len = fdata.write(n.data, file._pos, data)
+   file._pos = file._pos + len
    n.ctime = time()
    n.mtime = time()
    return len
 end
 
 local function append(file, data)
-   local n = file.node
+   local n = file._node
    local len = fdata.write(n.data, n.data.size, data)
-   n.ctime = time()
-   n.mtime = time()
+   t = time()
+   n.ctime = t
+   n.mtime = t
    return len
 end
 
 local function read(file, flag, len)
-   local n = file.node
+   local n = file._node
    local ret
    if flag == constants.READ_CHUNK then
-      ret = fdata.read(n.data, file.pos, len)
-      file.pos = file.pos + string.len(ret)
-      -- TODO pos update
+      ret = fdata.read(n.data, file._pos, len)
+      if ret then 
+         file._pos = file._pos + string.len(ret)
+      end
    elseif flag == constants.READ_ALL then
       ret = fdata.read(n.data, 0, n.data.size)
-      file.pos = n.data.size
+      file._pos = n.data.size
    elseif flag == constants.READ_LINE then
       -- TODO
       return nil, errno.EINVAL
@@ -39,9 +39,98 @@ local function read(file, flag, len)
       -- TODO
       return nil, errno.EINVAL
    end
-
+   n.atime = t   
    return ret 
 end
 
+local function seek(file, opt, offset)
+   local pos 
+   if opt == constants.SEEK_CUR then
+      pos = file._pos
+   elseif opt == constants.SEEK_END then
+      pos = file._node.data.size
+   elseif opt == constants.SEEK_SET then
+      pos = 0
+   end
+   newpos = pos + offset
+   if newpos < 0 then
+      return nil, errno.EINVAL
+   else
+      file._pos = newpos
+      return newpos
+   end
+end
+
+local function badf(file, ...)
+   return nil, errno.EBADF, errutils.str_error(errno.EBADF)
+end
+
+local function close(file)
+   file._fs[file._fd] = nil
+   file._node = nil
+   file._fs = nil
+   file._fd = nil
+   file.write = badf
+   file.read = badf
+   file.seek = badf 
+   file.flush = badf
+   file.close = badf
+   return true
+end
+
+function file_m.new(fs, node, flags, page_size)
+   local fd = utils.find_free_index(fs._files)
+   local file = {}
+   fs._files[fd] = file
+   file._node = node
+   file._fs = fs
+   file._fd = fd
+
+   local data = file._node.data
+   if flags.create then
+      local page_size = page_size or fs._cfg.page_size
+      data = fdata.new(page_size)
+   end
+   if flags.trunc then
+      fdata.truncate(data, 0)
+   end
+
+   -- Set write func
+   if flags.w then
+      if flags.append then
+         file.write = wrappers.write(append)
+      else 
+         file.write = wrappers.write(write)
+      end
+   else
+      file.write = badf
+   end
+
+   -- Set read func
+   if flags.r then
+      file.read = wrappers.read(read)
+   else
+      file.read = badf
+   end
+   
+   -- Seek
+   file.seek = wrappers.seek(seek)
+
+   -- TODO Mb add buffers
+   -- Flush
+   file.flush = function() end
+
+   -- Close
+   file.close = close
+
+   return file
+end
 
 return file_m
+
+
+
+
+
+
+
